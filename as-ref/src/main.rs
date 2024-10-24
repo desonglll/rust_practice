ecc97ddfad5fe25923119c75385220a6a1844bfd
mikeshinoda <53910940+desonglll@users.noreply.github.com>
2024-10-24 23:04:22 +0800

    Update

---
+++ as-ref/src/main.rs
@@ -0,0 +1,43 @@
+use std::ops::Deref;
+
+#[derive(Debug)]
+#[allow(unused)]
+struct Point {
+    x: i32,
+    y: i32,
+    message: String,
+}
+
+impl Deref for Point {
+    type Target = Point;
+
+    fn deref(&self) -> &Self::Target {
+        self
+    }
+}
+
+#[derive(Debug)]
+struct MyError;
+
+fn get_point(is: bool) -> Result<Point, MyError> {
+    if is {
+        Ok(Point {
+            x: 1,
+            y: 2,
+            message: String::from("hello"),
+        })
+    } else {
+        Err(MyError)
+    }
+}
+
+fn main() {
+    let a = 10;
+    println!("a: {}", a);
+    let mut binding = get_point(true);
+    let result = binding.as_deref().unwrap();
+    println!("result: {:?}", result);
+    let mut_result = binding.as_mut().unwrap();
+    mut_result.message = String::from("world");
+    println!("mut_result: {:?}", mut_result);
+}
--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -99,7 +99,8 @@ fn main() {
     let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
     let mut pixels: Vec<u8> = vec![0; bounds.0 * bounds.1];
 
-    let threads = 8;
+    let threads = num_cpus::get();
+    println!("threads: {}", threads);
     let row_per_band = bounds.1 / threads + 1;
     {
         let bands: Vec<&mut [u8]> = pixels.chunks_mut(row_per_band * bounds.0).collect();
